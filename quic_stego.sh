3a5e5a22b8d2c6ed70acc9d2b94b49878c21d4b9
Carlos Panduro <carlos_panduro@Pandu-MacBook-Pro.local>
2021-05-10 20:02:43 +0200

    changes

--- quic_stego.sh
+++ quic_stego.sh
@@ -31,13 +31,13 @@ set -- "${POSITIONAL[@]}" # restore positional parameters
 
 if [[ -z "${EXTRACT}" ]]
 then
-    echo "We are going to hide the ${SECRET} in ${IMAGE}"
-    python stego.py -i "${IMAGE}" -s secret "${SECRET}"
-    python examples/http3_server.py --certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem -v
+    echo "We are going to hide the ${SECRET} in ${IMAGE} and send it by quic"
+    python3 src/stego_lsb/stego.py -i "${IMAGE}" -s "${SECRET}"
+    python3 examples/http3_server.py --certificate tests/ssl_cert.pem --private-key tests/ssl_key.pem -v
 else
     echo "We are going to download and extract the ${IMAGE}'s secret"
-    python examples/http3_client.py --ca-certs tests/pycacert.pem https://localhost:4433/stego
-    python stego.py -E -i "${IMAGE}"
+    python3 examples/http3_client.py --ca-certs tests/pycacert.pem https://localhost:4433/stego  --output-dir=result_resources/
+    python3 src/stego_lsb/stego.py -E -i "${IMAGE}"
 fi
 
 
